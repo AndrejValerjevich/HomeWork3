defdc812bbf97317d748bfea186a8f74168962e4
AndrejValerjevich <andrejdenisiuk@yandex.ru>
2017-04-15 21:10:13 +0500

    Update animals.php

--- animals.php
+++ animals.php
@@ -31,19 +31,13 @@ $animals_array = [
 
 $double_name_animals = [];//Массив для названий животных, состоящих из двух слов
 
-$count = 0;
-$double_name_animals_count = [];//Массив для сохранения количества животных с именами, состоящими из двух слов - в каждом регионе
-
 foreach ($animals_array as $countries => $animal_mas) {
     foreach ($animal_mas as $value) {
         if (!stripos($value, " ") == false)//Проверяем строку на наличие пробела
         {
             $double_name_animals [] = $value;
-            $count++;
         }
     }
-    $double_name_animals_count [] = $count;
-    $count = 0;
 }
 
 $first_parts_of_names = [];//Массив для первых частей названий животных
@@ -61,7 +55,6 @@ foreach ($first_parts_of_names as $value) {
     $fantastic_animals [] = "$value"." $second_parts_of_names[$rand_key]";//Соединяем первую часть жиотного со второй, выбранной рандомно
     unset($second_parts_of_names[$rand_key]);//Удаляем уже использованную вторую часть из массива $second_parts_of_names[]
 }
-
 #endregion
 ?>
 <!DOCTYPE html>
@@ -90,16 +83,21 @@ foreach ($first_parts_of_names as $value) {
                 echo "$value<br>";
             }
             echo "<br><h2 class=\"main-container-fieldset-animals-h2\">Список фантастических зверей по странам!</h2>";
+            $is_first_item = true;
             foreach ($animals_array as $countries => $real_animals) {
                 echo "<h2>$countries</h2>";
                 foreach ($real_animals as $real_animals_value) {
                     foreach ($fantastic_animals as $fantastic_animals_value) {
                         if (stristr($real_animals_value, " ", true) == stristr($fantastic_animals_value, " ", true))//Проверка животных на принадлеэность к  региону
                         {
-                            echo "$fantastic_animals_value, ";
+                            if ($is_first_item == true) {
+                                echo "$fantastic_animals_value";
+                                $is_first_item = false;
+                            }
+                            else echo ", $fantastic_animals_value";
                         }
                     }
-                }
+                } $is_first_item = true;
             }
         }
         else {
@@ -107,7 +105,6 @@ foreach ($first_parts_of_names as $value) {
             echo "<h2 class=\"main-container-h2\">Животные!</h2>";
         }
         ?>
-
     </fieldset>
 </div>
 </body>
